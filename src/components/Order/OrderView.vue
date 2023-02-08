8eea31be7c57e5e44291ccb3d44dbc9317a39ffb
zmx <zmx@fashiontiy.com>
2023-02-08 17:24:07 +0800

    - 提交订单相关文件

---
+++ src/components/Order/OrderView.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
